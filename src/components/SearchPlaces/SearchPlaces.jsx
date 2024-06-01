92ab9fea7f51e8b70c0444f35191897f1cc9949f
zeanchook50 <zhiyanchook@gmail.com>
2024-06-01 11:54:57 +0800

    feat: adding button for adding plans

--- src/components/SearchPlaces/SearchPlaces.jsx
+++ src/components/SearchPlaces/SearchPlaces.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 import { searchPlaces, searchDetail } from "../../utilities/search-service";
+import dayjs from "dayjs";
+import { useRef } from "react";
 
-export default function SearchPlaces()
+export default function SearchPlaces({plannerDetails})
 {
     const [searchState, setSearchState] = useState("");
     const [resultState, setResultState] = useState("");
     const [detailResult, setDetailResult] = useState({})
+
+    const dropdown = useRef(null);
 
     const handleSubmit = async(e) =>
     {
@@ -20,11 +24,22 @@ export default function SearchPlaces()
     }
 
     // console.log(resultState)
+    console.log(plannerDetails)
 
     const handleAdd = () =>
     {
         console.log("adding")
     }
+
+    const handleClicker = () =>
+    {
+        dropdown.current.removeAttribute("open")
+    }
+
+    const ListofDates = () => plannerDetails?.map((item,idx) =>
+    {
+        return(<li key={idx} onClick={handleClicker}><a>{dayjs(item.date).format("DD-MM-YYYY")} </a></li>)
+    })
 
     const handleDetails = async(event) =>
     {
@@ -37,10 +52,16 @@ export default function SearchPlaces()
 
     console.log(detailResult)
 
+    const handleClick = (e) =>
+    {   
+        console.log(e.target.id)
+        console.log(detailResult[e.target.id])
+    }
+
     const resultDisplay = resultState?.predictions?.map((item,idx) => 
         {
-            return(
-            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-200 bg-base-100" key={idx}>
+            return(<div style={{display:"flex"}} key={idx}>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-200 bg-base-100" >
                 <div className="collapse-title text-xl font-medium" style={{display:"flex"}}
                 id={item.place_id}
                 onClick={handleDetails}
@@ -52,8 +73,13 @@ export default function SearchPlaces()
                 <div className="collapse-content"> 
                     <div>{Object.keys(detailResult).length !== 0  && (detailResult[item.place_id]?.formatted_address)}</div>
                     <div>⭐ {Object.keys(detailResult).length !== 0  && (detailResult[item.place_id]?.rating)}</div>
-                    {/* <button>add</button> */}
                 </div>
+                
+            </div>
+            <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none 
+            focus:ring-4 focus:ring-gray-300 font-medium rounded-lg 
+            text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 
+            dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" id={item.place_id} onClick={handleClick}>Add</button>
             </div>)
             // console.log(item)
         })
@@ -75,6 +101,20 @@ export default function SearchPlaces()
     </form>
 
     {resultState && resultDisplay}
+
+    
+
+    {plannerDetails && <details className="dropdown" ref={dropdown} id="dropdown">
+    <summary className="m-1 btn">Add to your plan?</summary>
+    <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+      <ListofDates/>
+    </ul>
+  </details>}
+
+
+
+
+
         
     </>)
 }
