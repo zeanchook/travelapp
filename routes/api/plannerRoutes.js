57ad09eb7f614d3117ad7756483be219c06994c2
zeanchook50 <zhiyanchook@gmail.com>
2024-06-03 14:15:55 +0800

    feat: added route for patching of location items between days and intraday

--- routes/api/plannerRoutes.js
+++ routes/api/plannerRoutes.js
@@ -2,10 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 const plannerController = require("../../controllers/api/plannerController");
+// create a new planner
 router.post("/create", plannerController.create);
+
+// get list of planner by user id 
 router.get("/get", plannerController.index);
+
+// get list of days in a planner by user id 
 router.get("/getDetails/:id",plannerController.getDetails);
 
+// get list of each of items in each day of planner by user id 
+router.get("/getEachDetails/:id",plannerController.getEachDetails);
+
+//add item into a day of itinerary
 router.post("/add/:planneritemsid/itinerary",plannerController.addtoItinerary);
+
+//patch ordering of day itinerary
+router.patch("/update/order",plannerController.patchItinerary);
+
+//patch itenerary to move item around.
+router.patch("/update/itinerary",plannerController.patchDaysItinerary);
+
+router.get("/testing",plannerController.testing);
 
 module.exports = router;
