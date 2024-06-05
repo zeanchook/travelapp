211970cbb425a13a4610ed88003b0b0bb0d017ab
zeanchook50 <zhiyanchook@gmail.com>
2024-06-05 10:52:42 +0800

    feat: added a heatmap button for table to view heatmap for specific trip

--- src/components/TableResults/TableResults.jsx
+++ src/components/TableResults/TableResults.jsx
@@ -1,6 +1,6 @@
 import CalenderPicker from "../CalenderPicker/CalenderPicker"
 import { useNavigate } from "react-router-dom"
-export default function TableResults({plannerList})
+export default function TableResults({plannerList, setheatMapDisplay, setselectedPlanner})
 {
     console.log(plannerList)
     const navigate = useNavigate();
@@ -16,12 +16,15 @@ export default function TableResults({plannerList})
     const TableRow = () => plannerList?.map((item,idx) =>
            {return(<tr className="bg-white dark:bg-gray-800" key={idx} >
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white hover:underline hover:cursor-pointer"
-                        name={item.id} onClick={handleClick} 
+                        name={item.plannerid} onClick={handleClick} 
                         >{item.title}</th>
                         <td className="px-6 py-4">{item.created_at}</td> 
                         <td className="px-6 py-4">{item.created_at}</td> 
                         <td className="px-6 py-4">{parseInt(item.dayslength)}</td> 
-                        <td className="px-6 py-4">Button?</td>                               
+                        <td className="px-6 py-4"><button onClick={(e) => 
+                            {setheatMapDisplay(true)
+                            setselectedPlanner({selected : item , type: "select"})}
+                            }>View HeatMap</button></td>                               
                     </tr>)
         })
     return(
