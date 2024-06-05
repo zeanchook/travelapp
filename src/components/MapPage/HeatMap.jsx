e5299fb06e7bb8c50448a516738dd6b841f61456
zeanchook50 <zhiyanchook@gmail.com>
2024-06-05 10:50:53 +0800

    feat: added data to integrate with heatmap

--- src/components/MapPage/HeatMap.jsx
+++ src/components/MapPage/HeatMap.jsx
@@ -6,27 +6,39 @@ import MapGL, {Source, Layer} from 'react-map-gl';
 import {heatmapLayer} from './map-style';
 import {myVariable} from "./datatest"
 import { getMapData } from "../../utilities/mapdata-service";
-
+import "./stylesheet.css"
 
 const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN; // Set your mapbox token here
 
 
-export default function HeatMap() {
+
+export default function HeatMap({selectedPlanner}) {
 console.log(myVariable)
 const [resultss,setResults] = useState("")
+console.log(selectedPlanner)
+console.log(resultss)
+// let data = ""
+
+
 
 useEffect(() => {
-  async function getPlannerList() {
-    let results = await getMapData();
+  async function getPlannerList(plannerid) {
+    let results = await getMapData(plannerid);
     console.log(results)
     setResults(results)
   }
-  getPlannerList();
-}, []);
+console.log(selectedPlanner)
+  if(selectedPlanner)
+  {
+    console.log(selectedPlanner)
+    // console.log(selectedPlanner.select)
+    // const { plannerid } = selectedPlanner.select
+    getPlannerList(selectedPlanner.selected.plannerid)}
+}, [selectedPlanner]);
 
   return (
-    
-      <MapGL style={{height:"100%"}}
+    <div style={{width: "50vw",height: "50vh"}}>      
+    <MapGL 
         initialViewState={{
           latitude: 40,
           longitude: -100,
@@ -41,8 +53,7 @@ useEffect(() => {
           </Source>
         )}
       </MapGL>
-   
-
+      </div>
   );
 }
 
