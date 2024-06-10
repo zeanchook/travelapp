ab0750f7ddad2ac69d33f8b50a01b85e2dbd1e59
zeanchook50 <zhiyanchook@gmail.com>
2024-06-10 23:52:27 +0800

    feat: commenting out for testing purpose

---
+++ src/components/MapPage/heatmap-service.js
@@ -0,0 +1,41 @@
+
+// const views = [{latitude: 6, longitude: -104.18},
+//     {latitude: 6, longitude: -88.63},
+//     {latitude: 6, longitude: -68.63},
+//     {latitude: 6, longitude: -48.63},
+//     {latitude: 6, longitude: -28.63},
+//     {latitude: 6, longitude: -0.63},
+//     {latitude: 6, longitude: 20.15},
+//     {latitude: 6, longitude: 40.15},
+//     {latitude: 6, longitude: 60.15},
+//     {latitude: 6, longitude: 80.15},
+//   ]
+
+
+// export const heatMapservice = (onSelectCity) =>
+// {
+    
+// }
+
+// // useEffect(() => {
+// //     const interval = setInterval(() => {
+// //       // setCounter((prevCounter) => prevCounter + 1);
+// //       console.log("hello")
+// //       console.log(viewState)
+      
+// //       const findidnex = views.findIndex(item => item.longitude === viewState.longitude)
+// //       console.log(findidnex)
+// //       console.log(views[findidnex+1])
+// //       if(findidnex !== views.length - 1)
+// //       {
+// //         // console.log(views[findidnex+1])
+// //         setViewState(views[findidnex+1])
+// //       }
+// //       else 
+// //       {
+// //         setViewState(views[0])
+// //       }
+// //       // setViewsIndex()
+// //     }, 10000);
+// //     return () => clearInterval(interval);
+// //   }, []);
