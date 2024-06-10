edbabba5f3e6daa364715a0ab6a9789a50dc77eb
zeanchook50 <zhiyanchook@gmail.com>
2024-06-10 23:49:56 +0800

    feat: refactor page and styling up

--- src/pages/Community/Community.jsx
+++ src/pages/Community/Community.jsx
@@ -1,11 +1,13 @@
 import { currentSelectedRange, loginSts} from "../../../atom"
 import {  useAtomValue } from "jotai"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { createPlanner, getStats } from "../../utilities/planner-service"
 import dayjs from "dayjs"
 import MarkerMap from "../../components/MarkerMap/MarkerMap"
 import { useNavigate } from "react-router-dom"
 import CalenderPicker from "../../components/CalenderPicker/CalenderPicker"
+import { ToastContainer, toast } from 'react-toastify';
+import LoadingPopup from "../../components/LoadingPopup/LoadingPopup"
 
 
 export default function Community()
@@ -13,24 +15,31 @@ export default function Community()
     const [user] = useAtomValue(loginSts)
     const [post , setPost] = useState("")
 
+    const [createDisplay , setcreateDisplay] = useState(true)
+
+  const [loadingMsg, setLoadingMsg ] = useState(false)
+
     const [newForm, setnewForm] = useState(false);
     const dateValue = useAtomValue(currentSelectedRange);
     const [formValue , setFormValue] = useState("")
+
+    const inputEl = useRef("");
 
     const numDays = parseInt(dayjs(dateValue.endDate).format('DD/MM/YYYY'))-parseInt(dayjs(dateValue.startDate).format('DD/MM/YYYY'));
 
 
     const navigate = useNavigate();
 
+    // console.log("dateValue",dateValue)
 
-    console.log(user)
+    // console.log(user)
 
     useEffect(() => {
 
        async function getPost()
        {
             const resposne = await getStats();
-            console.log(resposne)
+            // console.log(resposne)
 
         //    const reducedData = {}
            
@@ -64,7 +73,7 @@ export default function Community()
           }, []);
               
             
-            console.log(reducedData)
+            // console.log(reducedData)
             setPost(reducedData)
        }
        getPost()
@@ -72,34 +81,52 @@ export default function Community()
 
       const handleClick = (e,item) =>
       {
-        console.log(item.planner_id)
+        // console.log(item.planner_id)
         navigate(`/planner/${item.planner_id}`)
       }
 
       const handleUserClick = (e,item) =>
       {
-        console.log(item)
+        // console.log(item)
         navigate(`/usrprofile/${item.userid}`)
       }
 
       const handleCreatePlanner = () =>
       {
           setnewForm(true)
+          setcreateDisplay(false)
       }
 
       const handleChange = (e) =>
     {
-      setFormValue({title: e.target.value})
+      // e.preventDefault();
+      // e.stopPropagation();
+      // console.log(inputEl.current)
+      // // setFormValue(e.target.value)
+      inputEl.current = e.target.value
     }
 
     const handleSubmit = async(e) =>
     {
-      e.preventDefault();
-      console.log(formValue)
+      // e.preventDefault();
+      // e.stopPropagation();
+      // console.log(formValue)
 
-      console.log({...dateValue,...formValue})
-      const result = await createPlanner({...dateValue,...formValue,daysLength:numDays})
-      console.log(result)
+      // console.log(inputEl.current)
+      // console.log({...dateValue,...formValue})
+      if(dateValue.default === "no")
+      {
+        setLoadingMsg(true)
+        const result = await createPlanner({...dateValue,title: inputEl.current,daysLength:numDays})
+        // console.log(result)
+        setLoadingMsg(false)
+        navigate(`/planner/${result}`)
+      }
+      else
+      {
+        toast("Planner not created as date is default !");
+        }
+     
       // if(typeof(result) === "string")
       // {
       //   toast(result)
@@ -109,7 +136,7 @@ export default function Community()
       // }
     }
 
-      const Post = () => post && post?.sort((b,a) => 
+      const Post = post && post?.sort((b,a) => 
       {
         if(a.created_at > b.created_at)
           {
@@ -125,8 +152,7 @@ export default function Community()
           }
       }).map((item,idx) =>
         {
-            console.log(item)
-            console.log(item)
+          // console.log(item)
             return(
             <div key={idx} style={{backgroundColor:"",width:"600px",padding:"10px"}}>
               <div className="stats shadow"  style={{margin:"10px",display:'flex',
@@ -139,18 +165,18 @@ export default function Community()
                     <img src="https://images.twinkl.co.uk/tr/raw/upload/u/ux/earth-parents-wiki_ver_1.jpg" />
                     <div className="stat-title" style={{}}
                     
-                    >{item.name}</div>
+                    ><b>{item.name}</b></div>
                     {/* <div className="stat-title">{dayjs(item.created_at).format('DD-MMM-YY')}{dayjs().format('DD-MMM-YY') }</div> */}
                     {/* <div className="text-xs">{dayjs(item.created_at).format('DD-MMM-YY')}</div> */}
                   </div>
 
               <div className="stat-title" style={{cursor:"pointer"}}
               onClick={(e) => handleClick(e,item)}
-              >📍 {item.title} {"posted on "+dayjs(item.created_at).format('DD-MMM-YY')}</div>
+              >created 📍 <b>{item.title}</b> with <b>{item.data.length+" locations innit "}</b>  {" on "+dayjs(item.created_at).format('DD-MMM-YY')}</div>
              
             </div>
            <div style={{margin:"10px",marginBottom:"20px"}}>
-                      <MarkerMap mapSize={{width: "25vw",height: "30vh"}} type={{selected : item , type: "POST"}}/>
+                      <MarkerMap mapSize={{width: "25vw",height: "30vh"}} mapData={{selected : item , type: "POST"}}/>
            </div>
           
           </div>
@@ -159,39 +185,58 @@ export default function Community()
           </div>)
         })
     //   console.log(post)
-    return(<div style={{ display: 'flex', height: '100vh', flexDirection:"column", backgroundColor:"yellow", alignItems:"center" ,overflowY:"scroll"}}>
-      <div className="p-4 border-t mx-8 mt-2">
-            <button className="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2"
+    return(<div style={{ display: 'flex', height: '100vh', 
+    flexDirection:"column", backgroundColor:"yellow", alignItems:"center" ,
+    background:'url("https://i.redd.it/10e6ttd0fp4d1.jpeg")', backgroundSize:"cover"
+    ,overflowY:"scroll"}}>
+
+        {/* <img src="https://parade.com/.image/t_share/MTk3MDMzODkyNzE1MTc3Mjc5/how-long-does-it-take-to-get-to-the-moon.jpg"/> */}
+
+   
+      <div style={{padding:"40px"}}><b style={{color:"white",fontSize:"50px"}}>Explore Our Community </b></div>
+      <div className="divider" style={{color:"white",borderColor:"white"}}>OR</div>
+      {createDisplay && <div className="p-4 border-t mx-8 mt-2">
+            <button className="btn btn-active btn-primary"
                 onClick={handleCreatePlanner}
             >
-                Create a Planner</button>
-        </div>
-
+                Create One for yourself</button>
+        </div>}
+        
         {newForm &&
-        <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
-        <div className="mb-5">
-          <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Planner Name</label>
-          <input type="text" id="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        <form className="max-w-sm mx-auto" onSubmit={handleSubmit} style={{display:"flex",flexDirection:"column",alignContent:"center"}}>
+        <div className="mb-5" >
+          <label htmlFor="text" className="block mb-2 text-sm font-medium" style={{color:"white"}}>New Planner Name</label>
+          <input type="text" id="text" className="bg-gray-50 border border-gray-300 text-gray-900 
+          text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 
+          dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+          ref={inputEl}
           onChange={handleChange}
           required />
         </div>
 
         <div className="mb-5">
-          <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date Range</label>
+          <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          style={{width:"300px",backgroundColor:"",color:"white"}}
+          >Date Range</label>
           <CalenderPicker />
         </div>
-        <div className="mb-5">
+        <div className="mb-5" style={{color:"white"}}>
           <p>{dateValue.default === "no" && `Trip Length : ${numDays}`}</p>
         </div>
         
         <button type="submit" 
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Create</button>
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
+        focus:outline-none focus:ring-blue-300 font-medium 
+        rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        style={{textWrap:"nowrap"}}
+        >
+        Create that planner !</button>
       </form>
         }
         <div style={{display:"flex",flexDirection:"column",padding:"100px"}}>
-        <Post/>
+        {Post}
         </div>
-        
+        <ToastContainer />
+        {loadingMsg && <LoadingPopup/>}
     </div>)
 }
