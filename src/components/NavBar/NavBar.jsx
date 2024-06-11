de4f56aa6ad0ece23ceccda6e342a3c9e233a288
zeanchook50 <zhiyanchook@gmail.com>
2024-06-11 10:17:48 +0800

    feat: styling up navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,21 +9,30 @@ export default function NavBar() {
     logOut();
     setUser(null);
   };
-  console.log(user)
+
 
   const [ currentUser ] = user;
-  
   const routes = `/usrprofile/${currentUser.id}`
 
   return (
-    <nav style={{backgroundColor:"grey"}}>
-      <NavLink to="/">Home</NavLink>
-      &nbsp; | &nbsp;
-      <NavLink to={routes}>My Profile</NavLink>
-      &nbsp;&nbsp;
-      <Link to="" onClick={handleLogOut}>
-        Log Out {user[0].name}
-      </Link>
-    </nav>
+<div className="navbar bg-base-300 rounded-box">
+  <div className="flex-1 px-2 lg:flex-none">
+    <a className="text-lg font-bold">TRAAPP</a>
+  </div> 
+  <div className="flex justify-end flex-1 px-2">
+    <div className="flex items-stretch">
+      <a className="btn btn-ghost rounded-btn" href="/" >Community</a>
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn">Welcome, {user[0].name}</div>
+        <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+          <li><a href ={routes}>My Profile</a></li> 
+          {currentUser.usertype === "admin" && <li><a href ="/admin">Admin</a></li>} 
+          <li><a to="" onClick={handleLogOut}>Log Out</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
   );
 }
