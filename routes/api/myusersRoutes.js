ecde0c01e0eeb53bc9ead3273560e869837e5d56
zeanchook50 <zhiyanchook@gmail.com>
2024-06-12 15:30:35 +0800

    feat: added a middleware for myusersroutes for admin action to check only admin can perform this route

--- routes/api/myusersRoutes.js
+++ routes/api/myusersRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const myUserControl = require("../../controllers/api/myusersController");
+const usertype = require("../../config/usertype");
 
 router.post("/signup", myUserControl.create);
 router.post("/login", myUserControl.login);
@@ -9,8 +10,8 @@ router.get("/index", myUserControl.index);
 
 router.patch("/getUserDetails", myUserControl.getUserDetails);
 
-router.delete("/delete", myUserControl.deleteUser);
-router.patch("/updateUserLevel", myUserControl.updateUserLevel);
+router.delete("/delete", [usertype] ,myUserControl.deleteUser);
+router.patch("/updateUserLevel", [usertype] ,myUserControl.updateUserLevel);
 
 router.patch("/patchViewer", myUserControl.patchViewer);
 router.patch("/getViewer", myUserControl.getViewer);
